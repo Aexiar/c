8e914e802e5fd2894001a860439b84ffee500776
许大仙 <1900919313@qq.com>
2024-11-27 07:39:16 +0000

    2024年11月27日 15:39

--- docs/.vitepress/navbar/index.ts
+++ docs/.vitepress/navbar/index.ts
@@ -3,10 +3,10 @@ export const nav: DefaultTheme.NavItem[] = [
   { text: '🏠首页', link: '/' },
   { text: '🧑‍💻计组6件套', link: '/coa6/notes/', target: '_blank' },
   {
-    text: '⚙️编程语言', items: [
+    text: '🍉编程语言', items: [
       { text: 'c/c++', link: '/notes/' },
       { text: '前端', link: '/web-design/notes/', target: '_blank' },
-      { text: 'Java', link: '/java/notes/', target: '_blank' },
+      { text: '☕Java', link: '/java/notes/', target: '_blank' },
       { text: '大数据', link: '/big-data/notes/', target: '_blank' },
       { text: '云原生', link: '/linux/notes/', target: '_blank' },
       { text: 'Go', link: '/go/notes/', target: '_blank' },
