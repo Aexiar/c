5f677898b70ef8e4f497a921209325afb606e5de
许大仙 <1900919313@qq.com>
2024-11-27 07:31:31 +0000

    2024年11月27日 15:31

--- docs/.vitepress/navbar/index.ts
+++ docs/.vitepress/navbar/index.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.NavItem[] = [
   { text: '🏠首页', link: '/' },
-  { text: '计组6件套', link: '/coa6/notes/', target: '_blank' },
+  { text: '🧑‍💻计组6件套', link: '/coa6/notes/', target: '_blank' },
   {
     text: '编程语言', items: [
       { text: 'c/c++', link: '/notes/' },
